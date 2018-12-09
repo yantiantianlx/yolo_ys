b12838c86c353637e0df19ed07de876d05e9db76
Ryan <1018718155@qq.com>
2018-12-09 19:57:58 +0800

    my map.py today

--- tst/map.py
+++ tst/map.py
@@ -4,7 +4,9 @@ import torch
 
 def bbox_iou_x1y1x2y2(box1, box2):
     """
-    Returns the IoU of two bounding boxes
+    :param box1:
+    :param box2:
+    :return:
     """
     assert len(box1) > 0 and len(box2) > 0
     assert type(box1) == type(box2)
@@ -84,16 +86,16 @@ def mark_predict_tp_fp_in_one_image(predict_bbox_array, predict_conf_array, gt_b
 
         ious = bbox_iou_x1y1x2y2(predict_bbox, gt_bbox_array)
         iou_max = np.max(ious)
-        iou_max_idx = np.argmax(ious)
+        iou_max_gt_idx = np.argmax(ious)
 
         if iou_max > iou_threshold:
-            if gt_occupy[iou_max_idx] == 0:
-                tp[iou_max_idx] = 1
-                gt_occupy[iou_max_idx] = 1
+            if gt_occupy[iou_max_gt_idx] == 0:
+                tp[pre_idx] = 1
+                gt_occupy[iou_max_gt_idx] = 1
             else:
-                fp[iou_max_idx] = 1
+                fp[pre_idx] = 1
         else:
-            fp[iou_max_idx] = 1
+            fp[pre_idx] = 1
 
     conf = np.expand_dims(conf, axis=1)
     tp = np.expand_dims(tp, axis=1)
@@ -109,7 +111,7 @@ def calculate_recall_and_precision(conf_tp_fp_array, all_gt_num):
     :return:
     """
     conf = conf_tp_fp_array[:, 0]
-    sorted_ind = np.argsort(-conf, kind='mergesort')
+    sorted_ind = np.argsort(-conf, kind='heapsort')
     conf_tp_fp_array = conf_tp_fp_array[sorted_ind, :]
 
     tp = conf_tp_fp_array[:, 1]
@@ -123,33 +125,43 @@ def calculate_recall_and_precision(conf_tp_fp_array, all_gt_num):
     return recall, precision
 
 
-# def calculate_ap(recall, precision):
-#
-#     # correct AP calculation
-#     # first append sentinel values at the end
-#     mrec = np.concatenate(([0.], rec, [1.]))
-#     mpre = np.concatenate(([0.], prec, [0.]))
-#
-#     # compute the precision envelope
-#     for i in range(mpre.size - 1, 0, -1):
-#         mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
-#
-#     # to calculate area under PR curve, look for points
-#     # where X axis (recall) changes value
-#     i = np.where(mrec[1:] != mrec[:-1])[0]
-#
-#     # and sum (\Delta recall) * prec
-#     ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
-#     return ap
+def filling_precision(precision):
+    local_max_pre = 0.
+    for i in range(len(precision)-1, -1, -1):
+        prec = precision[i]
+        if prec < local_max_pre:
+            precision[i] = local_max_pre
+        if prec > local_max_pre:
+            local_max_pre = prec
+    return precision
+
+
+def calculate_ap(recall, precision):
+    mrec = np.concatenate(([0.], recall, [1.]))
+    mpre = np.concatenate(([1.], precision, [0.]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
 
 
 def calculate_map():
     pass
 
 
-def draw_plot(mrec, mprec, ap):
+def draw_plot(rec, prec, ap):
     import matplotlib.pyplot as plt
     if True:
+        mrec = np.concatenate(([0.], rec, [1.]))
+        mprec = np.concatenate(([1.], prec, [0.]))
         plt.plot(mrec, mprec, '-o')
         # add a new penultimate point to the list (mrec[-2], 0.0)
         # since the last line segment (and respective area) do not affect the AP value
@@ -178,44 +190,91 @@ def draw_plot(mrec, mprec, ap):
         plt.cla()  # clear axes for next plot
 
 
-
 if __name__ == '__main__':
-    conf_tp_fp_array = mark_predict_tp_fp_in_one_image()
+    def x1y1wh_to_x1y1x2y2(*args):
+        if len(args) == 1:
+            box = args[0]
+            assert isinstance(args[0], (list, np.ndarray, torch.Tensor))
+            list_flag = False
+            if isinstance(box, list):
+                list_flag = True
+                box = np.array(box)
+            if len(box.shape) == 1:
+                x1 = box[0]
+                x2 = box[0] + box[2]
+                y1 = box[1]
+                y2 = box[1] + box[3]
+                box[0], box[1], box[2], box[3] = x1, y1, x2, y2
+                return box.tolist() if list_flag else box
+            if len(box.shape) == 2:
+                x1 = box[:, 0]
+                x2 = box[:, 0] + box[:, 2]
+                y1 = box[:, 1]
+                y2 = box[:, 1] + box[:, 3]
+                box[:, 0], box[:, 1], box[:, 2], box[:, 3] = x1, y1, x2, y2
+                return box.tolist() if list_flag else box
+            raise Exception('check here, you get no output')
+
+
+    gt_im_1 = np.array([[100, 100, 200, 200], [300, 300, 320, 320], [400, 400, 500, 500]])
+    gt_im_2 = np.array([[100, 100, 200, 200], [300, 300, 320, 320], [400, 400, 500, 500]])
+    gt_im_3 = np.array([[100, 100, 200, 200], [300, 300, 320, 320], [400, 400, 500, 500]])
+
+    pre_im_1 = np.array([[0.95, 100, 100, 190, 190], [0.7, 300, 300, 321, 321], [0.8, 400, 400, 499, 499], [0.9, 10, 10, 20, 20]])
+    pre_im_2 = np.array([[0.95, 100, 100, 190, 190], [0.85, 300, 300, 302, 302], [0.8, 400, 400, 499, 499], [0.9, 10, 10, 20, 20], [0.3, 10, 10, 20, 20], [0.4, 10, 10, 20, 20], [0.3, 10, 10, 20, 20], [0.2, 10, 10, 20, 20]])
+    pre_im_3 = np.array([[0.95, 100, 100, 190, 190], [0.6, 300, 300, 321, 321], [0.8, 400, 400, 499, 499], [0.9, 10, 10, 20, 20], [0.3, 10, 10, 20, 20], [0.2, 10, 10, 20, 20]])
+
+    conf1 = pre_im_1[:, 0]
+    conf2 = pre_im_2[:, 0]
+    conf3 = pre_im_3[:, 0]
+
+    pre_im_1 = pre_im_1[:, 1:]
+    pre_im_2 = pre_im_2[:, 1:]
+    pre_im_3 = pre_im_3[:, 1:]
+
+    conf_tp_fp_array1 = mark_predict_tp_fp_in_one_image(pre_im_1, conf1, gt_im_1, iou_threshold=0.5)
+    conf_tp_fp_array2 = mark_predict_tp_fp_in_one_image(pre_im_2, conf2, gt_im_2, iou_threshold=0.5)
+    conf_tp_fp_array3 = mark_predict_tp_fp_in_one_image(pre_im_3, conf3, gt_im_3, iou_threshold=0.5)
+
+    conf_tp_fp_array = np.concatenate((conf_tp_fp_array1, conf_tp_fp_array2, conf_tp_fp_array3), axis=0)
+
+    recall, precision = calculate_recall_and_precision(conf_tp_fp_array, 9)
+
+    print(calculate_ap(recall, precision))
+
+    draw_plot(recall, precision, 0.0000234)
+
 
 
 
 
     # # targets are in cls, x, y, w, h
-    # target_ano = {'1.jpg': [[1, 100, 100, 100, 100], [1, 300, 300, 20, 20], [1, 400, 400, 100, 100]],
-    #               '2.jpg': [[1, 100, 100, 100, 100], [1, 300, 300, 20, 20], [1, 400, 400, 100, 100]],
-    #               '3.jpg': [[1, 100, 100, 100, 100], [1, 300, 300, 20, 20], [1, 400, 400, 100, 100]],
+    # target_ano = {'1.jpg': [[100, 100, 100, 100], [300, 300, 20, 20], [400, 400, 100, 100]],
+    #               '2.jpg': [[100, 100, 100, 100], [300, 300, 20, 20], [400, 400, 100, 100]],
+    #               '3.jpg': [[100, 100, 100, 100], [300, 300, 20, 20], [400, 400, 100, 100]],
     #               }
     #
     # # dets are in cls, confidence, x, y, w, h
     # dets = [
-    #     ['1.jpg', 1, 0.95, 100, 100, 90, 90],
-    #     ['1.jpg', 1, 0.7, 300, 300, 21, 21],
-    #     ['1.jpg', 1, 0.8, 400, 400, 99, 99],
-    #     ['1.jpg', 1, 0.9, 10, 10, 10, 10],
+    #     ['1.jpg', 0.95, 100, 100, 90, 90],
+    #     ['1.jpg', 0.7, 300, 300, 21, 21],
+    #     ['1.jpg', 0.8, 400, 400, 99, 99],
+    #     ['1.jpg', 0.9, 10, 10, 10, 10],
     #
-    #     ['2.jpg', 1, 0.95, 100, 100, 90, 90],
-    #     ['2.jpg', 1, 0.85, 300, 300, 2, 2],
-    #     ['2.jpg', 1, 0.8, 400, 400, 99, 99],
-    #     ['2.jpg', 1, 0.9, 10, 10, 10, 10],
-    #     ['2.jpg', 1, 0.3, 10, 10, 10, 10],
-    #     ['2.jpg', 1, 0.4, 10, 10, 10, 10],
-    #     ['2.jpg', 1, 0.3, 10, 10, 10, 10],
-    #     ['2.jpg', 1, 0.2, 10, 10, 10, 10],
+    #     ['2.jpg', 0.95, 100, 100, 90, 90],
+    #     ['2.jpg', 0.85, 300, 300, 2, 2],
+    #     ['2.jpg', 0.8, 400, 400, 99, 99],
+    #     ['2.jpg', 0.9, 10, 10, 10, 10],
+    #     ['2.jpg', 0.3, 10, 10, 10, 10],
+    #     ['2.jpg', 0.4, 10, 10, 10, 10],
+    #     ['2.jpg', 0.3, 10, 10, 10, 10],
+    #     ['2.jpg', 0.2, 10, 10, 10, 10],
     #
-    #     ['3.jpg', 1, 0.95, 100, 100, 90, 90],
-    #     ['3.jpg', 1, 0.6, 300, 300, 21, 21],
-    #     ['3.jpg', 1, 0.8, 400, 400, 99, 99],
-    #     ['3.jpg', 1, 0.9, 10, 10, 10, 10],
-    #     ['3.jpg', 1, 0.3, 10, 10, 10, 10],
-    #     ['3.jpg', 1, 0.2, 10, 10, 10, 10],
+    #     ['3.jpg', 0.95, 100, 100, 90, 90],
+    #     ['3.jpg', 0.6, 300, 300, 21, 21],
+    #     ['3.jpg', 0.8, 400, 400, 99, 99],
+    #     ['3.jpg', 0.9, 10, 10, 10, 10],
+    #     ['3.jpg', 0.3, 10, 10, 10, 10],
+    #     ['3.jpg', 0.2, 10, 10, 10, 10],
     # ]
-    #
-    # rec, prec, ap = det_eval(dets, target_ano, 1, 0.75, plot=True)
-    # print(rec)
-    # print(prec)
-    # print(ap)
+
